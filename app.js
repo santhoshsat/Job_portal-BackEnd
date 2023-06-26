f408bcb707c3024feb36fc2ab88c18bef22ea660
santhoshsat <santhoshsat002@gmail.com>
2023-06-26 08:58:29 +0530

    add models

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// app.js for job_portal
+const express = require('express');
+const mongoose = require('mongoose');
+const cors =require('cors');
+const app=express();
+const PORT =3500;
+ 
+app.use(cors());
+app.use(express.json())
+ //connect to mongodb database 
+ mongoose.connect('mongodb://localhost/jobDB')
+ const db =mongoose.connection;
+ db.on("error",(err)=>{
+    console.log(err)
+ })
+ 
+ db.once("open",()=>{
+     console.log("connected to db successfully")
+ })
+ app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+ })
