b5e3d512c6add205c02a77c516ac700d969ae60b
santhoshsat <santhoshsat002@gmail.com>
2023-06-26 09:25:27 +0530

    added controllers

---
+++ data/initialData.js
@@ -0,0 +1,69 @@
+const initialData = [
+    {
+        "jobTitle": "Software Engineer",
+        "jobID": "001",
+        "jobCompany": "ABC Technologies",
+        "jobLocation": "New York",
+        "jobPreference": "Experienced",
+        "jobSkills": "Java, Python, SQL"
+        },
+        {
+        "jobTitle": "Data Analyst",
+        "jobID": "002",
+        "jobCompany": "XYZ Corporation",
+        "jobLocation": "San Francisco",
+        "jobPreference": "Fresher",
+        "jobSkills": "SQL, Excel, Statistics"
+        },
+        {
+        "jobTitle": "Web Developer",
+        "jobID": "003",
+        "jobCompany": "PQR Solutions",
+        "jobLocation": "London",
+        "jobPreference": "Experienced",
+        "jobSkills": "HTML, CSS, JavaScript"
+        },
+        {
+        "jobTitle": "Graphic Designer",
+        "jobID": "004",
+        "jobCompany": "Design Studio",
+        "jobLocation": "Paris",
+        "jobPreference": "Fresher",
+        "jobSkills": "Adobe Photoshop, Illustrator"
+        },
+        {
+        "jobTitle": "Marketing Manager",
+        "jobID": "005",
+        "jobCompany": "Global Marketing Inc.",
+        "jobLocation": "Tokyo",
+        "jobPreference": "Experienced",
+        "jobSkills": "Digital Marketing, SEO"
+        },
+        {
+        "jobTitle": "Business Analyst",
+        "jobID": "006",
+        "jobCompany": "Consulting Group",
+        "jobLocation": "Sydney",
+        "jobPreference": "Experienced",
+        "jobSkills": "Data Analysis, Project Management"
+        },
+        {
+        "jobTitle": "Sales Executive",
+        "jobID": "007",
+        "jobCompany": "SalesForce Ltd.",
+        "jobLocation": "Chicago",
+        "jobPreference": "Fresher",
+        "jobSkills": "Communication, Negotiation"
+        },
+        {
+        "jobTitle": "UX/UI Designer",
+        "jobID": "008",
+        "jobCompany": "Digital Agency",
+        "jobLocation": "Berlin",
+        "jobPreference": "Experienced",
+        "jobSkills": "User Research, Wireframing"
+        }
+      
+]
+
+module.exports = initialData
